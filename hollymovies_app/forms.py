f1fca2a8c35b037faf340477de4e09dab4d527dc
David Simek <david.simek1998@gmail.com>
2023-05-28 12:59:00 +0200

    added validation to contact form

--- hollymovies_app/forms.py
+++ hollymovies_app/forms.py
@@ -1,8 +1,20 @@
 from django import forms
+from django.core.exceptions import ValidationError
+
+
+class BadWordCharField(forms.CharField):
+    def validate(self, value):
+        bad_words = ["zakazene", "david", "chatgpt"]
+
+        for bad_word in bad_words:
+            if bad_word in value:
+                raise ValidationError("Text cannot contains zakazane slovo")
+
+        return value
 
 
 class ContactForm(forms.Form):
     name = forms.CharField(max_length=50)
     email = forms.EmailField()
-    text = forms.CharField(widget=forms.Textarea(attrs={"class": "form-control"}))
-    priority = forms.IntegerField()
+    text = BadWordCharField(widget=forms.Textarea(attrs={"class": "form-control"}))
+    priority = forms.IntegerField(min_value=1, max_value=10)
